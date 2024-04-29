72d9cbdc06b093104dee0bf933f61fc84ab0a659
Suppa <suppa.bastien@gmail.com>
2024-04-29 17:54:11 +0200

    updated arrows and text design

---
+++ src/pages/notFound/NotFound.jsx
@@ -0,0 +1,20 @@
+import './notFound.scss'
+import Header from "../../components/header/Header";
+import { Link } from 'react-router-dom';
+import Footer from '../../components/footer/Footer'
+
+export default function NotFound() {
+	return (
+		<div className='notFound'>
+			<Header />
+
+			<div className="notFound_infos">
+				<h1 className='notFound_infos_title'>404</h1>
+				<p className='notFound_infos_content'>Oups! La page que vous demandez n'existe pas.</p>
+			</div>
+			<Link className='notFound_infos_return' to='/Home'>Retourner sur la page d'accueil</Link>
+
+			<Footer />
+		</div>
+	)
+}
